43323003e609ea3c725c18b91f2320d6f36dad5f
Hoai Nam NGUYEN <hibou107@gmail.com>
2018-05-28 10:14:51 +0200

    more generic

--- src/main/scala/com/hibou107/finance/CompanySimulator.scala
+++ src/main/scala/com/hibou107/finance/CompanySimulator.scala
@@ -2,18 +2,17 @@ package com.hibou107.finance
 
 object CompanySimulator {
 
-  def compute(sales: Double, expense: Double, myWifeAnnualSalary: Double) = {
+  def compute(sales: Double, expense: Double, otherIncome: Double, part: Double) = {
     val benefit = sales - expense
     val remuneration = benefit / 1.47
-    println(remuneration + myWifeAnnualSalary)
-    val ir = Simulator.computeIR((remuneration + myWifeAnnualSalary) * 0.9, 2.5)
-    remuneration + myWifeAnnualSalary - ir
+    val ir = Simulator.computeIR((remuneration + otherIncome) * 0.9, part)
+    remuneration + otherIncome - ir
 }
 
 def main(args: Array[String]): Unit = {
   (0 to 20) foreach { i =>
     val sale = 50000 + i * 5000
-    val revenu = compute(sale, 2000, 1500*12)
+    val revenu = compute(sale, 2000, 1500*12, 2.5)
     println(s"$sale,$revenu")
   }
 }
